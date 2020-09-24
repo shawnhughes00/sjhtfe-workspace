7d864e9528f801aaf9893253a4349ebd786e7c80
shawnhughes00 <71796736+shawnhughes00@users.noreply.github.com>
2020-09-24 10:02:18 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "prefix" {}
+variable "environment" {}
+variable "location" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-tfe-rg"
+  location = var.location
+
+  tags = {
+    "environment" = var.environment
+  }
+}
